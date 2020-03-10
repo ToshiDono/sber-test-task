71e560de4175abd56c1ea93034af89ff0d621028
Anton Karpenko <toshidono.it.work@gmail.com>
2020-03-10 07:30:13 +0300

    Added examples

---
+++ src/main/java/db/ConnectionH2.java
@@ -0,0 +1,99 @@
+package db;
+
+import java.sql.*;
+
+public class ConnectionH2 {
+    private Connection connection;
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+
+    //  Database credentials
+    static final String USER = "sa";
+    static final String PASS = "";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // STEP 2: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            System.out.println("Connected database successfully...");
+
+            // STEP 3: Execute a query
+            stmt = conn.createStatement();
+            String sql = "INSERT INTO Registration " + "VALUES (100, 'Zara', 'Ali', 18)";
+
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO Registration " + "VALUES (101, 'Mahnaz', 'Fatma', 25)";
+
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO Registration " + "VALUES (102, 'Zaid', 'Khan', 30)";
+
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO Registration " + "VALUES(103, 'Sumit', 'Mittal', 28)";
+
+            stmt.executeUpdate(sql);
+            System.out.println("Inserted records into the table...");
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch(SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch(Exception e) {
+            // Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            // finally block used to close resources
+            try {
+                if(stmt!=null) stmt.close();
+            } catch(SQLException se2) {
+            } // nothing we can do
+            try {
+                if(conn!=null) conn.close();
+            } catch(SQLException se) {
+                se.printStackTrace();
+            } // end finally try
+        } // end try
+        System.out.println("Goodbye!");
+    }
+
+
+
+
+//    public static void main(String[] args) {
+//        try {
+//            Class.forName("org.h2.Driver").newInstance();
+//            Connection conn = DriverManager.getConnection("jdbc:h2:test",
+//                    "sa", "");
+//            Statement st = null;
+//            st = conn.createStatement();
+//            st.execute("INSERT INTO TEST VALUES(default,'HELLO')");
+//            st.execute("INSERT INTO TEST(NAME) VALUES('JOHN')");
+//            String name1 = "Jack";
+//            String q = "insert into TEST(name) values(?)";
+//            PreparedStatement st1 = null;
+//
+//            st1 = conn.prepareStatement(q);
+//            st1.setString(1, name1);
+//            st1.execute();
+//
+//            ResultSet result;
+//            result = st.executeQuery("SELECT * FROM TEST");
+//            while (result.next()) {
+//                String name = result.getString("NAME");
+//                System.out.println(result.getString("ID")+" "+name);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+}
