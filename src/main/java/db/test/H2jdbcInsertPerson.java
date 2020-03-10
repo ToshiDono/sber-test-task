392424eb139a191598b2ae2eac32514cf4aa1c56
Anton Karpenko <toshidono.it.work@gmail.com>
2020-03-10 07:29:54 +0300

    Added examples without custom connections and statements

---
+++ src/main/java/db/test/H2jdbcInsertPerson.java
@@ -0,0 +1,72 @@
+package db.test;
+
+import models.Person;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class H2jdbcInsertPerson {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+
+    //  Database credentials
+    static final String USER = "sa";
+    static final String PASS = "";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // STEP 2: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            System.out.println("Connected database successfully...");
+
+            // STEP 3: Execute a query
+            stmt = conn.createStatement();
+            Person person1 = new Person("user1", 19);
+            String sql = "INSERT INTO PERSONS " + "VALUES ("+ person1.getUuid() +
+                    ", '" + person1.getName() + "', "+ person1.getAge() +")";
+            stmt.executeUpdate(sql);
+
+            Person person2 = new Person("user2", 91);
+            sql = "INSERT INTO PERSONS " + "VALUES ("+ person2.getUuid() +
+                    ", '" + person2.getName() + "', "+ person2.getAge() +")";
+            stmt.executeUpdate(sql);
+
+            Person person3 = new Person("user3", 31);
+            sql = "INSERT INTO PERSONS " + "VALUES ("+ person3.getUuid() +
+                    ", '" + person3.getName() + "', "+ person3.getAge() +")";
+            stmt.executeUpdate(sql);
+            System.out.println("Inserted records into the table...");
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch(SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch(Exception e) {
+            // Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            // finally block used to close resources
+            try {
+                if(stmt!=null) stmt.close();
+            } catch(SQLException se2) {
+            } // nothing we can do
+            try {
+                if(conn!=null) conn.close();
+            } catch(SQLException se) {
+                se.printStackTrace();
+            } // end finally try
+        } // end try
+        System.out.println("Goodbye!");
+    }
+}
